b8ce654d7781c7e848c693dd5958923231ef478a
sostema <sostema@gmail.com>
2022-08-06 00:43:39 +0300

    fix: rehaul of dataclass definition

--- src/data_hack_faker/dataclasses/person.py
+++ src/data_hack_faker/dataclasses/person.py
@@ -1,10 +1,4 @@
-from dataclasses import dataclass, field
-import datetime
-import random
-
-from faker import Faker
-
-fake_ru = Faker(locale="ru_RU")
+from dataclasses import dataclass
 
 
 @dataclass
@@ -12,11 +6,4 @@ class Person:
     first_name: str
     last_name: str
     age: int
-    job: str
-    birth_date: datetime.date
-    def __init__(self):
-        self.first_name = field(default_factory=fake_ru.first_name)
-        self.last_name = field(default_factory=fake_ru.last_name)
-        self.age = field(default=random.randint(0, 100))
-        self.job = field(default_factory=fake_ru.job)
-        self.birth_date = field(default_factory=fake_ru.date_of_birth)
+    phone_number: str
