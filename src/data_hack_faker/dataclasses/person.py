5da7fa4000dc396ef1bf4edadc3f5a98c0fbd6b0
sostema <sostema@gmail.com>
2022-08-05 20:29:24 +0300

    feat: proposed dataclasses format

---
+++ src/data_hack_faker/dataclasses/person.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass, field
+import random
+
+from faker import Faker
+
+fake_ru = Faker(locale="ru_RU")
+
+
+@dataclass
+class Person:
+    first_name: str = field(default_factory=fake_ru.first_name())
+    last_name: str = field(default_factory=fake_ru.last_name())
+    age: int = field(default=random.randint(0, 100))
+    job: str = field(default_factory=fake_ru.job())
