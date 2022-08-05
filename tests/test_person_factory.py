5d9f7c023db5aa1865441842019b74a6c9d76435
sostema <sostema@gmail.com>
2022-08-06 02:04:10 +0300

    feat: add benchmark of PersonFactory

--- tests/test_person_factory.py
+++ tests/test_person_factory.py
@@ -1,12 +1,24 @@
+from data_hack_faker.dataclasses import Person
 from data_hack_faker.factories import PersonFactory
 from pytest_check import check
 
 
-def test_person_factory():
-    person = PersonFactory()
+def check_person(person: Person):
     with check:
         assert isinstance(person.first_name, str)
         assert isinstance(person.last_name, str)
-        assert 18 < person.age < 100
+        assert 18 <= person.age <= 100
         assert len(person.phone_number) == 11
         assert person.phone_number[:4] == "8918"
+
+
+def test_person_factory():
+    person = PersonFactory()
+    check_person(person)
+
+
+def test_person_factory_benchmark(benchmark):
+    persons = benchmark(PersonFactory.create_batch, size=10_000)
+    for person in persons:
+        with check:
+            check_person(person)
