52562d353ba74405521c9c47e79d370df4d76814
Maxim Fedotov <mvfedotov_1@edu.hse.ru>
2022-08-06 12:45:10 +0300

    fix: format code

--- src/data_hack_faker/cli.py
+++ src/data_hack_faker/cli.py
@@ -1,15 +1,17 @@
-import typer
-from typing import Optional
 from pathlib import Path
+from typing import Optional
 
+import typer
 
 app = typer.Typer()
 
 
 @app.command()
-def run(size: int = typer.Option(1000, "--size", "-s"),
-        verbose: bool = typer.Option(False, "--verbose", "-v"),
-        out: Optional[Path] = typer.Option(None, "--output", "-o")):
+def run(
+    size: int = typer.Option(1000, "--size", "-s"),
+    verbose: bool = typer.Option(False, "--verbose", "-v"),
+    out: Optional[Path] = typer.Option(None, "--output", "-o"),
+):
     typer.echo(f"Table size: {size} rows")
     typer.echo(f"Verbosity: {verbose}")
     typer.echo(f"Output file: {out}")
