3812149a48db62a2b7dfc4e673cb8560dc31a9fb
sostema <sostema@gmail.com>
2022-08-06 16:45:47 +0300

    feat: add tests

--- tests/test_generate_table.py
+++ tests/test_generate_table.py
@@ -1,8 +1,16 @@
-from data_hack_faker.factories import CustomerFactory
-from src.data_hack_faker.sparktable import spark_inference
+from pathlib import Path
+
+from data_hack_faker import factories
+from data_hack_faker.sparktable import spark_inference
 
 
 def test_generate_table():
-    batch_size = 1_000
-    customers = CustomerFactory.create_batch(size=batch_size)
-    spark_inference.generate_table_from_list(customers)
+    batch_size = 100
+    for factory in [factories.CustomerFactory]:
+        instances = factory.create_batch(size=batch_size)
+        spark_inference.generate_table_from_list(
+            instances, file_format="parquet", save_path=Path("../.tmp/")
+        )
+        spark_inference.generate_table_from_list(
+            instances, file_format="orc", save_path=Path("../.tmp/")
+        )
