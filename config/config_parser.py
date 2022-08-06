b85d2d0652095a72e1e6d08a06ce895f0d0e6bda
ikiselewskii <i.kiselevsky@gmail.com>
2022-08-07 00:33:42 +0300

    fix: style changes

--- config/config_parser.py
+++ config/config_parser.py
@@ -1,12 +1,12 @@
 import ujson
 
-with open('company/config.json') as f:
+with open("company/config.json") as f:
     company_settings = ujson.load(f)
-with open('employee/config.json') as f:
+with open("employee/config.json") as f:
     employee_settings = ujson.load(f)
-with open('order/config.json') as f:
+with open("order/config.json") as f:
     order_settings = ujson.load(f)
-with open('product/config.json') as f:
+with open("product/config.json") as f:
     product_settings = ujson.load(f)
-with open('customer/config.json') as f:
+with open("customer/config.json") as f:
     customer_settings = ujson.load(f)
