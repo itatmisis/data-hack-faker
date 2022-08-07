9fad5f903d7437c9c7b6f75daf7bc659aa4c209f
sostema <sostema@gmail.com>
2022-08-07 11:06:27 +0300

    fix: hotfix

--- src/data_hack_faker/factories/company_factory.py
+++ src/data_hack_faker/factories/company_factory.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 from config import company_settings as settings
 from factory import Faker
 
@@ -12,14 +14,24 @@ class CompanyFactory(BaseFactory):
         model = Company
 
     if "name" in settings:
-        name = Faker(settings["name"]["provider"], **settings["name"]["kwargs"])
+        name = Faker(
+            settings["name"]["provider"],
+            **{
+                k: OrderedDict(v) if isinstance(v, dict) else v
+                for k, v in settings["name"]["kwargs"].items()
+            },
+        )
     else:
         name = Faker("company")
     location = Faker("administrative_unit")
     address = Faker("address")
     if "phone_number" in settings:
         phone_number = Faker(
-            settings["phone_number"]["provider'"], **settings["phone_number"]["kwargs"]
+            settings["phone_number"]["provider'"],
+            **{
+                k: OrderedDict(v) if isinstance(v, dict) else v
+                for k, v in settings["name"]["kwargs"].items()
+            },
         )
     else:
         phone_number = Faker("bothify", text="8918#######")
